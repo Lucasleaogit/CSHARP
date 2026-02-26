640f29d8591a805b705af663a59bdf4ec30908e3
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 16:44:39 -0300

    Projeto C#

---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿int qtdMin, qtdMax;
+double estoqueMedio;
+
+    Console.WriteLine("Digite a quantidade mínima: ");
+    qtdMin = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite a quantidade maxima: ");
+    qtdMax = int.Parse(Console.ReadLine());
+
+   estoqueMedio = (qtdMin + qtdMax) / 2.0;
+
+Console.WriteLine("Estoque médio: " + estoqueMedio);
