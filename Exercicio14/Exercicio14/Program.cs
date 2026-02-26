7f253a14b7e8f71515cde0ab6fa738ad404b6820
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:17:45 -0300

    Projeto C#

---
+++ Exercicio14/Exercicio14/Program.cs
@@ -0,0 +1,24 @@
+﻿int numero;
+int somaPares = 0;
+int somaImpares = 0;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+while (numero >= 0)
+{
+    if (numero % 2 == 0)
+    {
+        somaPares += numero;
+    }
+    else
+    {
+        somaImpares += numero;
+    }
+
+    Console.Write("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Soma dos pares: " + somaPares);
+Console.WriteLine("Soma dos ímpares: " + somaImpares);
