5e494cbfad1a206d49cf35fbf834e8de78e1b347
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 16:43:18 -0300

    Projeto C#

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+        Console.Write("Digite o valor total do salario: R$ ");
+        double totalVendas = double.Parse(Console.ReadLine());
+
+        double comissao = totalVendas * 0.05;
+
+        Console.WriteLine($"Comissão do vendedor: R$ {comissao:F2}");
