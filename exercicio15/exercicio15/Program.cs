fe10220bd3c4ca08626d261c4df75ce9e43ee391
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:16:53 -0300

    Projeto C#

---
+++ exercicio15/exercicio15/Program.cs
@@ -0,0 +1,21 @@
+﻿double indice;
+
+Console.Write("Digite o índice de poluição: ");
+indice = double.Parse(Console.ReadLine());
+
+if (indice >= 0.5)
+{
+    Console.WriteLine("Notificar os grupos 1, 2 e 3");
+}
+else if (indice >= 0.4)
+{
+    Console.WriteLine("Notificar os grupos 1 e 2");
+}
+else if (indice >= 0.3)
+{
+    Console.WriteLine("Notificar o grupo 1");
+}
+else
+{
+    Console.WriteLine("Índice aceitável, nenhuma notificação necessária");
+}
