c16d16f9f2f0c3abb2a5e6782d9467f669b1f4ef
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 16:44:15 -0300

    Projeto C#

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite o valor a ser convertido: ");
+double valorDolar = double.Parse(Console.ReadLine());
+
+double cotacaoHoje = 5.27;
+double valorReal = valorDolar * cotacaoHoje;
+
+Console.WriteLine($"Convertido! Seu valor em dólar (${valorDolar:F2}) será: R${valorReal:F2}");
