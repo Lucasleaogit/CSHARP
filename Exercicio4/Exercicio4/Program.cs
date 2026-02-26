1836ba297ad43fbf874b882a455fef6fb2831b7a
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 16:42:31 -0300

    Projeto C#

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite a distaçia a rodar: ");
+double km = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quanto você abasteceu: ");
+double litros = double.Parse(Console.ReadLine());
+
+double consumo = (km / litros);
+Console.WriteLine($"O consumo do carro é: {consumo:F2} litros por quilometro");
